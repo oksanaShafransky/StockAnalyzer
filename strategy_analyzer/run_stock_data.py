f138560c87af4e343c2a69270e19f9052b638ddf
Oksana Shafransky <oksi78@yahoo.com>
2024-06-03 10:14:44 +0300

    send trend change notification

--- strategy_analyzer/run_stock_data.py
+++ strategy_analyzer/run_stock_data.py
@@ -6,7 +6,7 @@ from strategy_analyzer.stock_manager import StockManager
 if __name__ == "__main__":
     end_date = datetime.datetime.now()
     start_date = end_date - datetime.timedelta(days=5*365)
-
+    to_email = ['vladiks@gmail.com', 'oksi78@yahoo.com', 'oksi.shafransky@gmail.com']
     tickers = ['AAPL','ABNB','AEG','SEDG',]
     stock_manager = StockManager()
     stocks_default = stock_manager.get_stocks_by_ticker(tickers, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     stock_manager.run_strategy(stocks_default, sma_strategy_default)
     stock_manager.calc_profit_for_stocks(stocks_default)
     stock_manager.visualize(stocks_default, sma_strategy_default)
-
+    stock_manager.send_mail_with_trend_change_signals(stocks_default, '2024-05-28', to_email)
     #stock_manager.send_email_with_plots(stocks_default, sma_strategy_default, ['vladiks@gmail.com','oksi78@yahoo.com','oksi.shafransky@gmail.com'])
